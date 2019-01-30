0de3ddc6bb62a75f9d9b565fb5e6b76445b06858
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 01:35:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def reverse_each_word(sentence1)
  end 
  
  def reverse_each_word(sentence2)
-    array = sentence1.split(" ")
+ array = sentence2.split(" ")
  returning = []
  array.collect do |word|
    returning << word.reverse
