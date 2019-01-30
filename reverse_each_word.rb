9823ee6fbe5f1b33350180342fb86af04102fb40
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 01:16:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word("Hello there, and how are you?")
   "Hello there, and how are you?".split
-  .reverse_each_word(sentence1)
+  "Hello there, and how are you?".reverse_each_word(sentence1)
 end
 
   
